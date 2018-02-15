6b0e7dd4f2f4b824a4f6b96d701ac68e6bf431ed
Jesse Weaver <jesse354@gmail.com>
2018-02-15 11:48:20 -0600

    added tools

--- monstergame.py
+++ monstergame.py
@@ -1,13 +1,48 @@
-import sys
 import pygame
 
 
-pygame.init()
+def main():
+    width = 500
+    height = 500
+    white_color = (0, 0, 0)
+    blue_color = (97, 159, 182)
+    bg = ()
 
-screen = pygame.display.set_mode((500, 500))
-screen.fill((0, 0, 0))
+    pygame.init()
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption('My Game')
+    clock = pygame.time.Clock()
 
-while True:
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        sys.exit()
+    # Game initialization
+
+    stop_game = False
+    while not stop_game:
+        for event in pygame.event.get():
+
+            # Event handling
+
+            if event.type == pygame.QUIT:
+                stop_game = True
+
+
+        # Game logic
+
+        # Draw background
+        screen.fill(white_color)
+
+        # Game display
+
+        pygame.display.update()
+        clock.tick(60)
+
+    pygame.quit()
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
